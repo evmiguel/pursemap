df2b6493c30d9f78ab3b5afe45338fcd21703f71
Erika Miguel <erika@erikamiguel.com>
2024-04-07 19:51:24 -0400

    Add prisma adapter and shadow database

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,11 @@
-import NextAuth from 'next-auth';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from "next-auth/providers/google";
+import type { Adapter } from 'next-auth/adapters';
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from '@/lib/prisma';
 
-const options = {
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
@@ -10,6 +14,7 @@ const options = {
   ],
   debug: false
 }
-const handler = NextAuth(options);
+
+export const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
