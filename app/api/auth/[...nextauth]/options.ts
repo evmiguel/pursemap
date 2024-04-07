86adaac3d1dbd900cbae7cb3be43ed3937481dc6
Erika Miguel <erika@erikamiguel.com>
2024-04-07 19:59:14 -0400

    Extract authOptions into own file

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,16 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import type { Adapter } from 'next-auth/adapters';
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from '@/lib/prisma';
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma) as Adapter,
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID as string,
+        clientSecret: process.env.GOOGLE_SECRET as string
+      }),
+    ],
+    debug: false
+  }
