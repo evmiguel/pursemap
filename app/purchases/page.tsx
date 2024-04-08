ac3865fae2f1eafb5c1ecd89f2dc9d7d23c82002
Erika Miguel <erika@erikamiguel.com>
2024-04-07 23:20:26 -0400

    Get transactions

--- app/purchases/page.tsx
+++ app/purchases/page.tsx
@@ -10,6 +10,7 @@ import { cookies } from 'next/headers';
 import { getIronSession } from 'iron-session';
 import { plaidClient, sessionOptions } from "@/lib/plaid";
 import { SessionData } from "../api/plaid/exchange-public-token/route";
+import { TransactionsGetRequest } from "plaid";
 
 async function getPurchases(email: string) {
     const user = await prisma.user.findFirst(
@@ -26,16 +27,45 @@ async function getPurchases(email: string) {
     return user?.purchases;
 }
 
-async function getBalances() {
+async function getTransactions() {
     const session = await getIronSession<SessionData>(cookies(), sessionOptions);
-    const access_token = session.access_token;
+    const accessToken = session.access_token;
   
-    if (!access_token) {
+    if (!accessToken) {
         return [];
     }
   
-    const response = await plaidClient.accountsBalanceGet({ access_token });
-    console.log(response.data)
+    const request: TransactionsGetRequest = {
+        access_token: accessToken,
+        start_date: '2018-01-01',
+        end_date: '2024-04-07'
+    };
+
+    try {
+        const response = await plaidClient.transactionsGet(request);
+        let transactions = response.data.transactions;
+        const total_transactions = response.data.total_transactions;
+        // Manipulate the offset parameter to paginate
+        // transactions and retrieve all available data
+        while (transactions.length < total_transactions) {
+          const paginatedRequest: TransactionsGetRequest = {
+            access_token: accessToken,
+            start_date: '2018-01-01',
+            end_date: '2024-04-07',
+            options: {
+              offset: transactions.length
+            },
+          };
+          const paginatedResponse = await plaidClient.transactionsGet(paginatedRequest);
+          transactions = transactions.concat(
+            paginatedResponse.data.transactions,
+          );
+        }
+        console.log(transactions[0])
+      } catch (error) {
+        console.error(error)
+      }
+      
 }
 
 export default async function Page() {
@@ -44,6 +74,8 @@ export default async function Page() {
     if (session === null) {
         redirect('/api/auth/signin');
     }
+
+    await getTransactions()
 
     const purchases = await getPurchases(session?.user?.email as string) || [];
     
