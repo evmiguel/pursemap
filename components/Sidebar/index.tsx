a15ef1cdeede655f6a1df1e6a1a3c1fc1b1b5974
Erika Miguel <erika@erikamiguel.com>
2024-04-09 17:01:38 -0400

    Setting active state for chosen view

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { FilterContext } from "@/app/filter-provider";
 import { Purchase } from "@prisma/client";
 import Analysis from "../Analysis";
@@ -12,15 +12,16 @@ type SidebarProps = {
 
 export default function Sidebar(props: SidebarProps) {
     const { className, purchases } = props;
+    const [active, setActive] = useState('week');
 
     const context = useContext(FilterContext);
 
     return (
         <ul className={className}>
-            <li className="inline-block md:block uppercase underline mb-2"><Link href="#" onClick={() => context.setPurchaseFilter('week')}>This week</Link></li>
-            <li className="inline-block md:block uppercase underline mb-2"><Link href="#" onClick={() => context.setPurchaseFilter('month')}>This month</Link></li>
-            <li className="inline-block md:block uppercase underline mb-2"><Link href="#" onClick={() => context.setPurchaseFilter('year')}>This year</Link></li>
-            <li className="inline-block md:block uppercase underline mb-2"><Link href="#" onClick={() => context.setPurchaseFilter('all')}>All</Link></li>
+            <li className={`inline-block md:block uppercase mb-2 ${active == 'week' && 'underline'}`}><Link href="#" onClick={() => { context.setPurchaseFilter('week'); setActive("week")}}>This week</Link></li>
+            <li className={`inline-block md:block uppercase mb-2 ${active == 'month' && 'underline'}`}><Link href="#" onClick={() => { context.setPurchaseFilter('month'); setActive("month")}}>This month</Link></li>
+            <li className={`inline-block md:block uppercase mb-2 ${active == 'year' && 'underline'}`}><Link href="#" onClick={() => { context.setPurchaseFilter('year'); setActive("year")}}>This year</Link></li>
+            <li className={`inline-block md:block uppercase mb-2 ${active == 'all' && 'underline'}`}><Link href="#" onClick={() => { context.setPurchaseFilter('all'); setActive("all")}}>All</Link></li>
             <Analysis purchases={purchases} />
         </ul>
     )
